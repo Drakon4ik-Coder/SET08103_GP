8588dd94ba13517f828274ee827ab53f81b5ed16
Alex180504 <125076392+Alex180504@users.noreply.github.com>
2024-03-02 18:22:11 +0000

    Added commend-line UI

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -1,5 +1,8 @@
 package com.napier.sem;
 
+import org.hibernate.HibernateException;
+
+import java.io.*;
 import java.util.List;
 
 public class Main {
@@ -15,23 +18,50 @@ public class Main {
             System.out.println(city.toString());
         }*/
 
-        // Country Report for region(limit N)
-        List<Country> topNPopulatedCountries = getTopPopulatedCountries(10);
-        for (Country country : topNPopulatedCountries) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            System.out.println("Enter:\n1 - Display the countries in the world by population\n2 - Display the countries in a continent organised by population\n3 - Display the countries in a region organised by population\nexit - Quit the program");
             try {
-                System.out.println("Code: " + country.getCode() + ", Name: " + country.getName() + ", Continent: " + country.getContinent() + ", Region: " + country.getRegion() + ", Population: " + country.getPopulation() + ", Capital: " + country.getCapital().getName());
+                String input = reader.readLine();
+                int limit = 0;
+                switch (input) {
+                    case "1":
+                        // Country Report for world
+                        System.out.println("Enter the maximum number of results (0 for no limit): ");
+                        limit = Integer.parseInt(reader.readLine());
+
+                        List<Country> topPopulatedCountries = getTopPopulatedCountries(limit);
+                        for (Country country : topPopulatedCountries) {
+                            try {
+                                System.out.println("Code: " + country.getCode() + ", Name: " + country.getName() + ", Continent: " + country.getContinent() + ", Region: " + country.getRegion() + ", Population: " + country.getPopulation() + ", Capital: " + country.getCapital().getName());
+                            }
+                            catch (NullPointerException e) {
+                                continue;
+                            }
+                        }
+
+                    case "exit":
+                        break;
+                    default:
+                        System.out.println("Error: invalid input");
+                }
             }
-            catch (NullPointerException e) {
+            catch (IOError e) {
+                throw new RuntimeException(e);
+                //continue;
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number");
                 continue;
             }
-        }
-        // Country Report for world
-        List<Country> topPopulatedCountries = getTopPopulatedCountries(0);
-        for (Country country : topPopulatedCountries) {
-            try {
-                System.out.println("Code: " + country.getCode() + ", Name: " + country.getName() + ", Continent: " + country.getContinent() + ", Region: " + country.getRegion() + ", Population: " + country.getPopulation() + ", Capital: " + country.getCapital().getName());
+            catch (HibernateException e) {
+                System.out.println("Database connection error");
+                break;
             }
-            catch (NullPointerException e) {
+            catch (Exception e) {
+                //System.out.println("Database error");
+                e.printStackTrace();
                 continue;
             }
         }
@@ -42,6 +72,8 @@ public class Main {
         String hqlQuery = "FROM Country ORDER BY population DESC";
         return DBReader.queryCountries(hqlQuery, limit, null, null);
     }
+
+
 
 
 
