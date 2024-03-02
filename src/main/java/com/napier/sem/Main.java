f94689dfa14e15a66807bf5bc3bad8b497c4d257
Alex180504 <125076392+Alex180504@users.noreply.github.com>
2024-03-02 18:26:52 +0000

    Code cleanup

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -8,16 +8,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        /*List<Country> countries = DBReader.queryCountries("FROM Country", 10);
-        for (Country country : countries) {
-            System.out.println(country.toString());
-        }
-
-        List<City> cities = DBReader.queryCities("FROM City", 10);
-        for (City city : cities) {
-            System.out.println(city.toString());
-        }*/
-
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         while (true) {
