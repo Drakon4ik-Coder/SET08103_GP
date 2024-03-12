73830ac55e4d8e29b2f5ba4e35ae27e40d63254b
Illia <illiadovho@gmail.com>
2024-03-12 20:15:42 +0000

    Removed unwanted attributes from Country.java

--- src/main/java/com/napier/sem/Country.java
+++ src/main/java/com/napier/sem/Country.java
@@ -22,39 +22,12 @@ public class Country {
     @Column(name = "Region")
     private String region;
 
-    @Column(name = "SurfaceArea")
-    private double surfaceArea;
-
-    @Column(name = "IndepYear")
-    private Short indepYear;
-
     @Column(name = "Population")
     private int population;
-
-    @Column(name = "LifeExpectancy")
-    private Double lifeExpectancy;
-
-    @Column(name = "GNP")
-    private Double gnp;
-
-    @Column(name = "GNPOld")
-    private Double gnpOld;
-
-    @Column(name = "LocalName")
-    private String localName;
-
-    @Column(name = "GovernmentForm")
-    private String governmentForm;
-
-    @Column(name = "HeadOfState")
-    private String headOfState;
 
     @ManyToOne
     @JoinColumn(name = "Capital")
     private City capital;
-
-    @Column(name = "Code2")
-    private String code2;
 
     @Override
     public String toString() {
@@ -63,17 +36,8 @@ public class Country {
                 ", name='" + name + '\'' +
                 ", continent=" + continent +
                 ", region='" + region + '\'' +
-                ", surfaceArea=" + surfaceArea +
-                ", indepYear=" + indepYear +
                 ", population=" + population +
-                ", lifeExpectancy=" + lifeExpectancy +
-                ", gnp=" + gnp +
-                ", gnpOld=" + gnpOld +
-                ", localName='" + localName + '\'' +
-                ", governmentForm='" + governmentForm + '\'' +
-                ", headOfState='" + headOfState + '\'' +
                 ", capital=" + (capital != null ? capital.getName() : "null") + // Display capital information
-                ", code2='" + code2 + '\'' +
                 '}';
     }
 }
