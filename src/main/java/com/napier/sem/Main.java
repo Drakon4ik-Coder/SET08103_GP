012b449d78ef8fa61c6bb77a021e7746d89d4200
Stepan <stepan.demyanenko30@gmail.com>
2024-03-13 17:10:23 +0000

    Tests for cities queries

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -9,6 +9,21 @@ public class Main {
             System.out.println(capCity.toStringCapital());
         }
 
+        List<City> citiesWorld = DBReader.queryCities(Query.City.getWorldDesc(), 10);
+        for (City city : citiesWorld) {
+            System.out.println(city.toString());
+        }
+
+        List<City> citiesContinent = DBReader.queryCities(Query.City.getContinentDesc("Asia"), 10);
+        for (City city : citiesContinent) {
+            System.out.println(city.toString());
+        }
+
+        List<City> citiesCountry = DBReader.queryCities(Query.City.getCountryDesc("France"), 10);
+        for (City city : citiesCountry) {
+            System.out.println(city.toString());
+        }
+
         System.out.println("2");
     }
 }
