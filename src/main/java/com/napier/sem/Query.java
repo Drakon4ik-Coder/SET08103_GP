08539309b5cc28cb234f32842d649d5355abcfba
Stepan <stepan.demyanenko30@gmail.com>
2024-03-13 17:09:51 +0000

    Queries for Cities World Desc, Continent Desc and Region Desc

--- src/main/java/com/napier/sem/Query.java
+++ src/main/java/com/napier/sem/Query.java
@@ -20,4 +20,16 @@ public class Query {
             return String.format(REGION_DESC, region);
         }
     }
+    public static class City {
+        private static String WORLD_DESC = "FROM City c ORDER BY population DESC";
+        private static String CONTINENT_DESC = "FROM City c WHERE c.country.continent = '%s' ORDER BY c.population DESC";
+        private static String COUNTRY_DESC = "FROM City c WHERE c.country.name = '%s' ORDER BY c.population DESC";
+
+        public static String getWorldDesc(){ return WORLD_DESC; }
+        public static String getContinentDesc(String continent){ return String.format(CONTINENT_DESC, continent);}
+        public static String getCountryDesc(String country){
+            return String.format(COUNTRY_DESC, country);
+        }
+    }
+
 }
