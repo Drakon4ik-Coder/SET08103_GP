edb0a91911818fe5cd52478c3c3018aa3554db1d
Alex180504 <125076392+Alex180504@users.noreply.github.com>
2024-04-21 21:04:12 +0100

    Added country table integration test

--- src/test/java/com/napier/sem/IntegrationTests.java
+++ src/test/java/com/napier/sem/IntegrationTests.java
@@ -1,5 +1,4 @@
 package com.napier.sem;
-import org.eclipse.jetty.websocket.common.events.AbstractEventDriver;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -7,12 +6,9 @@ import org.hibernate.cfg.Configuration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 import org.hibernate.query.Query;
-
-import java.io.File;
+import java.math.BigDecimal;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class IntegrationTests {
@@ -45,20 +41,56 @@ public class IntegrationTests {
     }
 
     @Test
-    public void testCityTable() {
+    public void testCityTablePresenceAndStructure() {
         try (Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
-            Query query = session.createQuery("FROM City");
-            List<City> cities = query.list();
-            assertFalse(cities.isEmpty(), "City table is empty");
-            for (City city : cities) {
-                assertNotNull(city.getCountry(), "City country should not be null");
-                assertNotNull(city.getDistrict(),"City district should not be null");
+            Query query = session.createNativeQuery("SELECT * FROM city");
+            List<Object[]> cityRows = query.list();
+            assertFalse(cityRows.isEmpty(), "City table is empty");
+            for (Object[] row : cityRows) {
+                assertEquals(5, row.length, "Incorrect number of columns in City table");
+                assertTrue(row[0] instanceof Integer, "ID column should be an Integer");
+                assertTrue(row[1] instanceof String, "Name column should be a String");
+                assertTrue(row[2] instanceof String, "CountryCode column should be a String");
+                assertTrue(row[3] instanceof String, "District column should be a String");
+                assertTrue(row[4] instanceof Integer, "Population column should be an Integer");
             }
             transaction.commit();
         } catch (Exception ex) {
-            fail("Failed to check city table presence and structure: " + ex.getMessage());
+            fail("Failed to check City table presence and structure: " + ex.getMessage());
         }
     }
+
+    @Test
+    public void testCountryTablePresenceAndStructure() {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Query query = session.createNativeQuery("SELECT * FROM country");
+            List<Object[]> countryRows = query.list();
+            assertFalse(countryRows.isEmpty(), "Country table is empty");
+            for (Object[] row : countryRows) {
+                assertEquals(15, row.length, "Incorrect number of columns in Country table");
+                assertTrue(row[0] instanceof String, "Code column should be a String");
+                assertTrue(row[1] instanceof String, "Name column should be a String");
+                assertTrue(row[2] instanceof String, "Continent column should be a String");
+                assertTrue(row[3] instanceof String, "Region column should be a String");
+                assertTrue(row[4] instanceof BigDecimal, "SurfaceArea column should be a BigDecimal");
+                assertTrue(row[5] == null || row[5] instanceof Short, "IndepYear column should be an Short or null");
+                assertTrue(row[6] instanceof Integer, "Population column should be an Integer");
+                assertTrue(row[7] == null || row[7] instanceof BigDecimal, "LifeExpectancy column should be a BigDecimal or null");
+                assertTrue(row[8] == null || row[8] instanceof BigDecimal, "GNP column should be a BigDecimal or null");
+                assertTrue(row[9] == null || row[9] instanceof BigDecimal, "GNPOld column should be a BigDecimal or null");
+                assertTrue(row[10] instanceof String, "LocalName column should be a String");
+                assertTrue(row[11] instanceof String, "GovernmentForm column should be a String");
+                assertTrue(row[12] == null || row[12] instanceof String, "HeadOfState column should be a String or null");
+                assertTrue(row[13] == null || row[13] instanceof Integer, "Capital column should be an Integer or null");
+                assertTrue(row[14] instanceof String, "Code2 column should be a String");
+            }
+            transaction.commit();
+        } catch (Exception ex) {
+            fail("Failed to check country table presence and structure: " + ex.getMessage());
+        }
+    }
+
 
 }
